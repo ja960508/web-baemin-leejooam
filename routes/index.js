aa13b6d98c67dcd0fc60c4ab706f293c31f9881e
jooam <lord0325@naver.com>
2022-07-07 17:32:16 +0900

    refactor: index 라우터 비즈니스 로직 제거
    - sessionDBManager에서 반환받은 값만 사용하도록 변경

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const cookieParser = require('../lib/cookie_parser');
 const sessionDBManager = require('../lib/session_db_manager');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let user;
-
-  if (req.headers.cookie) {
-    const cookies = cookieParser(req.headers.cookie);
-
-    user = sessionDBManager.checkSessionId(cookies['sessionId']);
-  }
-
-  res.render('index', { title: 'My배민', user });
+  res.render('index', {
+    title: 'My배민',
+    user: sessionDBManager.checkSessionId(req.headers.cookie),
+  });
 });
 
 module.exports = router;
